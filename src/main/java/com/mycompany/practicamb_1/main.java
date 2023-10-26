543ca81d9c7a56e9a57f90e0b4e81b4b29cb40ef
Jonathan <52703336+Gabo-Dev@users.noreply.github.com>
2023-10-26 12:47:30 +0200

    -- actualizando interfaz

--- src/main/java/com/mycompany/practicamb_1/main.java
+++ src/main/java/com/mycompany/practicamb_1/main.java
@@ -3,7 +3,9 @@ package com.mycompany.practicamb_1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -15,55 +17,81 @@ import org.apache.solr.common.SolrInputDocument;
 public class main {
 
     public static void main(String[] args) throws FileNotFoundException, SolrServerException, IOException {
-        String dir = System.getProperty("user.dir");
-        String filename = dir + "\\src\\corpus\\CISI.ALL";
-        Scanner sc = new Scanner(new File(filename));
+        Scanner sc;
+        String filename, dir;
         String state = "b", index = null, title = null, author = null, text = null,
                 line, parts[];
+         //ArrayList<String> desc = null;
+        
+        
+        int opc = JOptionPane.showConfirmDialog(null, "¿Insertar documento?");
+        if (opc == 0) {
+            // Reset Colecciones
+            DeletingAllDocuments();
+            dir = System.getProperty("user.dir");
+            filename = dir + "\\src\\corpus\\CISI.ALL";
+            sc = new Scanner(new File(filename));
 
-        // Reset Colecciones
-        DeletingAllDocuments();
-
-        //  leemos
-        while (sc.hasNextLine()) {
-            state = "p";
-            line = sc.nextLine();
-            //System.out.println(line);
-            //  indice
-            if (line.startsWith(".I", 0)) {
-                indexaDocumento(index, title, author, text);
-                parts = line.split(" ");
-                index = parts[1];
-                System.out.println(index);
-            }
-            if (line.startsWith(".T", 0)) {
-                title = sc.nextLine();
-            }
-            if (line.startsWith(".A", 0)) {
-                author = sc.nextLine();
-            }
-            if (line.startsWith(".W", 0) || state == "READING") {  //texto 
-                text = sc.nextLine();
-                state = "READING";
-            }
-            if (state == "READING") {  //texto del documento 
-                String part = sc.nextLine();
-                if (part.startsWith(".X", 0)) {
-                    state = "IGNORING";
-                } else {
-                    text = text + part;
+            //  leemos
+            while (sc.hasNextLine()) {
+                state = "p";
+                line = sc.nextLine();
+                //System.out.println(line);
+                //  indice
+                if (line.startsWith(".I", 0)) {
+                    indexaDocumento(index, title, author, text);
+                    parts = line.split(" ");
+                    index = parts[1];
+                    //  System.out.println(index);
                 }
-                if ((line.startsWith(".X", 0)) || state == "IGNORING") {  //referencias cruzadas del documento     
-                    //no hago nada
-                }    else {
-                    //aquí tampoco
+                if (line.startsWith(".T", 0)) {
+                    title = sc.nextLine();
                 }
+                if (line.startsWith(".A", 0)) {
+                    author = sc.nextLine();
+                }
+                if (line.startsWith(".W", 0) || "READING".equals(state)) {  //texto 
+                    text = sc.nextLine();
+                    state = "READING";
+                }
+                if ("READING".equals(state)) {  //texto del documento 
+                    String part = sc.nextLine();
+                    if (part.startsWith(".X", 0)) {
+                        state = "IGNORING";
+                    } else {
+                        text = text + part;
+                    }
+                    if ((line.startsWith(".X", 0)) || "IGNORING".equals(state)) {  //referencias cruzadas del documento     
+                        //no hago nada
+                    } else {
+                        //aquí tampoco
+                    }
+                }
+            }
+            indexaDocumento(index, title, author, text);
+        } else {
+            opc = JOptionPane.showConfirmDialog(null, "¿Consultar documento?");
+            if (opc == 0) {
+                //  Consulta */
+               
+                dir = System.getProperty("user.dir");
+                filename = dir + "\\src\\corpus\\CISI.QRY";
+                sc = new Scanner(new File(filename));
+                while (sc.hasNextLine()) {
+                    line = sc.nextLine();
+                    //System.out.println(line);
+                    //  indice
+                    if (line.startsWith(".W", 0)) {
+                        //desc.add(line);
+                        text = sc.nextLine();
+                        searchDocuments(text);
+                    }
+                }
+               
+                //searchDocuments(title);
             }
         }
-        indexaDocumento(index, title, author, text);
-        
-        //  Consulta 
-        searchDocuments();
+
     }
 
     private static void DeletingAllDocuments() {
@@ -95,16 +123,16 @@ public class main {
         solr.commit();
     }
 
-    private static void searchDocuments() throws SolrServerException, IOException {
+    private static void searchDocuments(String desc) throws SolrServerException, IOException {
         HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/micoleccion").build();
-        SolrQuery query  =  new SolrQuery();
-        query.setQuery("*");
+        SolrQuery query = new SolrQuery();
+        query.setQuery("text");
         QueryResponse rsp = solr.query(query);
-        SolrDocumentList docs  = rsp.getResults();
-        int tm = docs.size();
-        System.out.println("Tamaño de la consulta: "+ tm);
-        for (int i = 0; i < tm; i++) {
-            System.out.println(docs.get(i));
-        }
+        SolrDocumentList docs = rsp.getResults();
+         int tm = docs.size();
+        System.out.println("Tamaño de la consulta: " + tm);
+        /*for (int i = 0; i < tm; i++) {
+            System.out.println(docs.get(i).get("title"));
+        }*/
     }
 }
